5a1e22829e442c742d06940d999294ae4fc415c9
Aaksha Sharma <60940606+Aaksha-sharma@users.noreply.github.com>
2021-07-28 13:04:59 +0530

    Create solution.cpp

---
+++ solution.cpp
@@ -0,0 +1,23 @@
+//this is function for given problem.Happy Learning!!
+void forkCPP(int N){
+    
+    // Your code here
+    if (N%3==0 && N%5==0)
+    {
+        cout<<"Fork CPP";
+    }
+    else if (N%3==0)
+    {
+        cout<<"Fork";
+    }
+    else if (N%5==0)
+    {
+        cout<<"CPP";
+    }
+    else
+    {
+        cout<<endl;
+    }
+  
+    cout << endl;  
+}
